9a94e23ea486599bda6b1b46a3498b3f7289b272
Arseny Zarya <arsenij.zarja@gmail.com>
2021-10-28 21:31:49 +0300

    libraries homework

---
+++ app/src/main/java/com/example/libraries/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.libraries
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import com.airbnb.lottie.LottieAnimationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val lottieAnimationView: LottieAnimationView = findViewById(R.id.lottieAnimationView1)
+        val crashButton = Button(this)
+        crashButton.text = "Test Crash"
+        crashButton.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+
+        addContentView(crashButton, ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT))
+    }
+}
